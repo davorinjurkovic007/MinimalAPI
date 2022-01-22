1a548c0742ba3ce2034b465507f404ac1746c7f5
davorinjurkovic007 <davorin.jurkovic@gmail.com>
2022-01-22 21:11:30 +0100

    Swagger integration

---
+++ DevNot21/DevNot21/Model/Role.cs
@@ -0,0 +1,10 @@
+﻿namespace DevNot21.Model
+{
+    public record Role(int RoleId, string RoleName, int GroupID)
+    {
+        public bool HasRole(int access, int roleID)
+        {
+            return roleID == (access & roleID);
+        }
+    }
+}
--- DevNot21/DevNot21/Program.cs
+++ DevNot21/DevNot21/Program.cs
@@ -1,6 +1,62 @@
+
+using DevNot21.Model;
+using Microsoft.OpenApi.Models;
+/// <summary>
+/// https://medium.com/geekculture/end-to-end-project-with-minimal-api-in-asp-net-core-6-0-f32eaca9334d
+/// </summary>
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api", Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id ="Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
+
 var app = builder.Build();
 
+if(app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1"));
+}
+
 app.MapGet("/", () => "Hello World!");
+
+app.Urls.Add("https://localhost:7214");
+
+app.MapGet("/GetRoles", (Func<List<Role>>)(() => new()
+{
+    new(1, "Admin", 1),
+    new(2, "User", 1),
+    new(2, "Worker", 1)
+}));
 
 app.Run();
